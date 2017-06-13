146d1f0293d9fa5732a948e00bbc5d82a23c59f6
Otis <otiskopp@yahoo.com>
2017-06-12 22:18:30 -0400

    Add files via upload

---
+++ MarioKart.rb
@@ -0,0 +1,341 @@
+mario_kart_intro = "
+MMMMMMMM               MMMMMMMM                                      iiii
+M:::::::M             M:::::::M                                     i::::i
+M::::::::M           M::::::::M                                      iiii
+M:::::::::M         M:::::::::M
+M::::::::::M       M::::::::::M  aaaaaaaaaaaaa  rrrrr   rrrrrrrrr  iiiiiii    ooooooooooo
+M:::::::::::M     M:::::::::::M  a::::::::::::a r::::rrr:::::::::r i:::::i  oo:::::::::::oo
+M:::::::M::::M   M::::M:::::::M  aaaaaaaaa:::::ar:::::::::::::::::r i::::i o:::::::::::::::o
+M::::::M M::::M M::::M M::::::M           a::::arr::::::rrrrr::::::ri::::i o:::::ooooo:::::o
+M::::::M  M::::M::::M  M::::::M    aaaaaaa:::::a r:::::r     r:::::ri::::i o::::o     o::::o
+M::::::M   M:::::::M   M::::::M  aa::::::::::::a r:::::r     rrrrrrri::::i o::::o     o::::o
+M::::::M    M:::::M    M::::::M a::::aaaa::::::a r:::::r            i::::i o::::o     o::::o
+M::::::M     MMMMM     M::::::Ma::::a    a:::::a r:::::r            i::::i o::::o     o::::o
+M::::::M               M::::::Ma::::a    a:::::a r:::::r           i::::::io:::::ooooo:::::o
+M::::::M               M::::::Ma:::::aaaa::::::a r:::::r           i::::::io:::::::::::::::o
+M::::::M               M::::::M a::::::::::aa:::ar:::::r           i::::::i oo:::::::::::oo
+MMMMMMMM               MMMMMMMM  aaaaaaaaaa  aaaarrrrrrr           iiiiiiii   ooooooooooo
+
+KKKKKKKKK    KKKKKKK                                              tttt           !!!
+K:::::::K    K:::::K                                           ttt:::t          !!:!!
+K:::::::K    K:::::K                                           t:::::t          !:::!
+K:::::::K   K::::::K                                           t:::::t          !:::!
+KK::::::K  K:::::KKK  aaaaaaaaaaaaa  rrrrr   rrrrrrrrr   ttttttt:::::ttttttt    !:::!
+K:::::K K:::::K     a::::::::::::a r::::rrr:::::::::r  t:::::::::::::::::t    !:::!
+K::::::K:::::K      aaaaaaaaa:::::ar:::::::::::::::::r t:::::::::::::::::t    !:::!
+K:::::::::::K                a::::arr::::::rrrrr::::::rtttttt:::::::tttttt    !:::!
+K:::::::::::K         aaaaaaa:::::a r:::::r     r:::::r      t:::::t          !:::!
+K::::::K:::::K      aa::::::::::::a r:::::r     rrrrrrr      t:::::t          !:::!
+K:::::K K:::::K    a::::aaaa::::::a r:::::r                  t:::::t          !!:!!
+KK::::::K  K:::::KKKa::::a    a:::::a r:::::r                  t:::::t    tttttt !!!
+K:::::::K   K::::::Ka::::a    a:::::a r:::::r                  t::::::tttt:::::t
+K:::::::K    K:::::Ka:::::aaaa::::::a r:::::r                  tt::::::::::::::t !!!
+K:::::::K    K:::::K a::::::::::aa:::ar:::::r                    tt:::::::::::tt!!:!!
+KKKKKKKKK    KKKKKKK  aaaaaaaaaa  aaaarrrrrrr                      ttttttttttt   !!!"
+
+user_mario = "
+──────────────███████──███████
+──────────████▓▓▓▓▓▓████░░░░░██
+────────██▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░██
+──────██▓▓▓▓▓▓████████████░░░░██
+────██▓▓▓▓▓▓████████████████░██
+────██▓▓████░░░░░░░░░░░░██████
+──████████░░░░░░██░░██░░██▓▓▓▓██
+──██░░████░░░░░░██░░██░░██▓▓▓▓██
+██░░░░██████░░░░░░░░░░░░░░██▓▓██
+██░░░░░░██░░░░██░░░░░░░░░░██▓▓██
+──██░░░░░░░░░███████░░░░██████
+────████░░░░░░░███████████▓▓██
+──────██████░░░░░░░░░░██▓▓▓▓██
+────██▓▓▓▓██████████████▓▓██
+──██▓▓▓▓▓▓▓▓████░░░░░░████
+████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+██████▓▓▓▓▓▓▓▓██░░░░░░████████
+──██████▓▓▓▓▓▓████████████████
+────██████████████████████▓▓▓▓██
+──██▓▓▓▓████████████████▓▓▓▓▓▓██
+████▓▓██████████████████▓▓▓▓▓▓██
+██▓▓▓▓██████████████████▓▓▓▓▓▓██
+██▓▓▓▓██████████──────██▓▓▓▓████
+██▓▓▓▓████──────────────██████
+──████"
+
+user_luigi = "
+______██████████████
+-____██▓▓▓▓▓▓▓▓▓▓▓▓████
+-__██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██
+-__██████░░░░██░░██████
+██░░░░████░░██░░░░░░░░██
+██░░░░████░░░░██░░░░░░██
+-__████░░░░░░██████████
+-__██░░░░░░░░░░░░░██
+_____██░░░░░░░░░██
+-______██░░░░░░██
+-____██▓▓████▓▓▓█
+-_██▓▓▓▓▓▓████▓▓█
+██▓▓▓▓▓▓███░░███░
+-__██░░░░░░███████
+-____██░░░░███████
+-______██████████
+-_____██▓▓▓▓▓▓▓▓▓██
+-_____█████████████"
+
+user_toad = "
+░░░░████████
+░░███░░▓▓▓▓███
+░░█░░░░▓▓▓▓░░█
+░█░░░░▓▓▓▓▓▓░░█
+██░░░▓▓░░░░▓▓░██
+█▓▓▓▓▓░░░░░░▓▓▓█
+█▓░░▓▓░░░░░░▓▓▓█
+█░░░░▓░░░░░░▓▓░█
+█░░░░▓▓░░░░▓▓░░█
+█▓░░▓▓▓▓▓▓▓▓▓░░█
+█▓▓▓████████▓▓░█
+░███░░█░░█░░███
+░░█░░░█░░█░░░█
+░░█░░░░░░░░░░█
+░░░█░░░░░░░░█
+░░░░████████
+"
+
+user_win = "
+██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗██╗███╗   ██╗██╗
+╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║    ██║██║████╗  ██║██║
+ ╚████╔╝ ██║   ██║██║   ██║    ██║ █╗ ██║██║██╔██╗ ██║██║
+  ╚██╔╝  ██║   ██║██║   ██║    ██║███╗██║██║██║╚██╗██║╚═╝
+   ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝██║██║ ╚████║██╗
+   ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═╝                                                                            \|__|
+"
+
+user_kart = "
+                                  ZMMM@W@MMB
+                                8M@BMarZMB0MMX          :S2@MSX
+                               M@88W  2  W@8WM8       78S2@MMS2B8
+                              M@ZZ@  rZ   WW8WM2     ZSXMM7irMWra@;
+                             ;M888W  0    .M80@M   ,8X8MXr7Xr;ZMr2WX
+                             MBaa8X ZS     M80BMM  a7WB;7XXSXX;ZWr2@,
+                            XMB88aS W2    iM80BWM 20M8;Xr::7SXX:M0XaM
+                          WM82ZB@MM  aMM  MB80BWMZMaWX77.  rXX7iMBMMMr
+                           MMW72ZBMM.  ; 2M000BBMMiiM:SX   rSX;ZW;7XMM
+                             MMZSa8@MMS;8MB000BB@M,;@s8ar.;XX77MrS22XM
+                             B00M8aa0@M@8800BBBB@MXaMSa22;XX7rMSX22a2Mi
+                            2, 2MMBW8ZW@W0000BBWWMX@MMaa8i;i;08X22aZ8Mr
+                            M.  :B28@0ZBW@W00BWW@MSMBWM2SSMMMMrSZBMMZ8
+                           .0  MM  7MMM0B@@w0bwwmm SW80MMWX:,MM@@x    .S
+                           XB aMM ,788BM@W@M@WW@MMM .2MMXZM@ZM;      i2B
+                           Mr aM  X. ,iZMMWB@@w@M@Mii  :           iaWM;
+                         Mr:;rii ;7 MBM.aMMMW@@@mmm2z2s:        .;8MWa
+                         , ,:iraXZ  MMM:;X8WMMMMMMaB@02Z,.:ii7Z0MMMa7
+                        7:::::;X8M ;MM7rXa82ZMMMMM iZ@B088B@WB82XBS8
+                        X:::iirX8WZ.7X;7aBZZMMMMM;rZ;r8B@@8ss0b@WMZ
+                        Z;i;;;rS000BS;XaBZ8MMZBM@;;aXX7;77;:2M2:ri
+                        rar;rrXZ08aaaaZ080MMS2XSWM7XaSSSSa80ZirZMi
+                         8BX7XaW802rX20ZWMMBBW@XW@@rzazz0@0;XBM8
+                          aMMMM0BMMMMMMaM@WB08XSWBMa2WWWW0WMMa
+                           7;XMMWW@MMMZ28282S2;WMMMMMMMMMMM:
+                           2;i2MMMMMMBSMW8SSa0MMMM; ,
+                            X2SiZW@Wa2MM0W@MMMMM2
+             ia          rZ0WSXSaa220WMM0WMM@82a0@MMMMMZ
+           , rM        BMMWBBWWMMBB@@wbwmm@a2aZZZZ8880WMM:
+            :BW      iMWZ2aZZ8B0WW@@@mmmm82azzz0@MMMMMWBMMM
+           .7@W    iMM00aaZZZ0BWB@MMMMr SZZ8ZXZWi    .M@BWMM
+           ,i8M   ;;X0B2aZZZ0BBWWMM8   @b88zxxw :  ZM  MWBWMM
+       ..: . SBW  Xr0W82ZZ80WWWWWMM   MW8ZZSSW  MMMM2M XMBB@MS
+      , 8Mi .0ZZ0XMMZZ2SZa0BW@@8wm   M@88Za2aM  M aa MMrMBWWMM
+     : ..  ,r2aaZ7 MMMM8ZZBWMWZZMB  ZM88ZZZZ8M aM .,.S7a@BWW@M:
+     :   ,.,:i22S7 XMB0BZ8WM@Z8MM   M0ZZZ8880MZ:M      M00B@MMX
+     rr:,,:;r2aaa0, MWWBBBMMZZBM8  rM8888880B0Z;    X@@08880wm
+     , ;Z88ZZ8ZZ2Z2 MMMMMM@WBWWM   MW8888000SXZWMMMMW0ZZa222ZM
+     ri  rXaS:2ZaaarMMM@WWWaaZ@M   M0888008ZZ8ZZZZZaS2SS2BMBMBMi
+      ,72S2aZ00ZZZZSMBBBBWMW00BMM  M8880B0ZZZ8Z2SS2ZWW8MMW0a@ZM
+      r  X28ZX8WBMW@@wwwwwww000wmm MW0BBBZZZ27rrZZ082S7MB28ZBMM
+      7SaWBBWMMS MM@WWWWWWWBBBBWMM 2MBB0X2SXZ@MMM872aa8MZS288MM
+        SMMM@;   WM@WWWW@WWBWBBWMM  MMMM8B@WWZa8@MMa8ZW@@ms0zmw
+                 :MMWWWW@W@WWWW@MMXX,ZMB07@WBWS2SZ@W088MMM7W8MW
+                  ZMM@@@b80ww@MMa a i  MW  iXa.MWM; .:SBM@X0r@i
+                   iMM@B0BWWWMMi,8Mi0020WW.:,; BMMi .,,BZ7;  M:
+                     MM@0BBWMM iBMB 7r70MMairX rMM,.i777rX: MM0
+                      ;MMMMMM2 BMMM ;:.:MM:  ,i    rr:,,,iXMMMa
+                          XWM: MMMMS7i  MB  ar    ii,,,,,::iMM:
+                      7        XMM@rMM2  X  0MMZ;,i .,::i;;7MWMM       . r7iS2SX.
+                      S.     i;i, ,7Z0MM@W, .MMMMMW .,iir7XZM0BMMMMMMZiZ7i. ,,,,X;
+                       a7irr7Sa8ar    rZWBS, :MMMMM:.:;r7X8MWBBBB0BBM  ..      .i@
+                        8MW0M@BZS;7  .SSMB@Ma.  MMMMMa2ZBMMB8BWBBBBWM .          ;:
+                       MMa   2a22X0  .ZZMB00MMSi,Xi.8WB0WBWW80BWBBBMM : ...  .:i  X
+                       BMi     .rM0  iBWWW@@@@mwbb8xrrszwwwww0w@MM@MB i      :@m: iMW
+                    Z00;WZ7i  .:7WBr 2WWWBWWWWWWWWZ8@MMMM@@wwzbb0mmmm r   . :0aW  SM2WW
+                   .i;:7M  S0@@b@8ZWMMXMWWW@BWWWB@a;2ZSSS22aW@7r:i  82MZ    SSBMi70Z72WM
+                  i;: ;00     ,r02SXaaaMWBWBZBW@@m87xzwzzazbwx8x.@mmmwb@8   82MMM7SXSSa8M
+                   WraBBMS,   .;WaZr7aMMMMMW0BBBB@ZSXSZW0BWZS0WZWMW8SaZWM@.S:M  Z;ZXS287WM
+                    W80ZM ;Z8aa@WaaXZMMMMMMMM@W088Z2S2ZW8XXaa280WMZ7S22a@MXirM  ;XZXS20r8WM
+                  .M80a7a   .ra8..:aWXS     MMB88a02Z7;a@Za2Z080WMXXSS2aB,a MMMM S2SSa078aWM
+        i.       rMW2W2  2   .aMrS@MM ;X2 XS@MB2a8Wai,,,@xazz80b@M;XSS2ZS7:8MMM7 Z;22Z8SZZ0MM
+       ,W@MMMM: 2MB7 XBrSBM8a0Wa7BMMX::. ,@a@MW0BZXZi,:iM8aZ880WMZ7222aZi2SM0Z0 X;r7a8SZZ0i.;8
+       :;   XMMMZ2a2SZM@00B@MMM  r  7r  7W8 ZM8a2aZ08S2B@2Z800WWMX2Z88Za2SBaSMX.7i;7S0XZZW WZ;X
+       :a7i.  :MB2Z2aSX8WMMMMMM  rB;2Si 8B  i2Waa8aaa80BaZ8000W@@az88za02w0wm@ r.;;7X8aaaB iMM8i
+       MWWW0Z7,iZ2222200ZX08;7X  .W :  Z@Z;r7 B00ZZZZZZZZZ880BMMMWMMBaZWMMMMMr7r2a808a0a2W  Z2MMi
+     rMWaa222aaaiSS22BB8ZZZ8ZrXZ  @  ,ZWZi;iX.;M0ZZZZZZZZZZ80MMBBWZaWW0Z22ZZWSaaSXXXSaZBa0;MMZ MM.
+     MMMB00888888a2X7SZ88800BaBM  M7riaai;;22r,0M0Z88ZZ8800WMWa88;:iirS8aZ0@  .rSXXX2ZZ808 ZMM;:0M
+       XMMMMMMMMMM@@@@02z2a;B0BM2 aSXBW:rr7Z;r;.8WZa22222ZBM8aZBr;777iSMZM8.iri:rXXSaZZZZW2 M ri:aM
+          ,, .rSMMMMMM@@wwba080wm iMW: :iXai8 2;,B2SSS222a0MaZ8W;r7Xr;M0WXi;X2X7r7S2aaZZZ8M aM.;r;M
+        :aXBM      XXXaBWWWWB8Za@  M@WS :2iSM .;iW2SS2aaaZ8M8Z8B;;ri:aMZ:;;2WXB7XSSS2ZZaZZBa Ma:r.M8
+      :@bzzwbbmmx: :0a0a08WMMMW0B  00Z0MWBWMM 80Z022S22aaZZM0aZ0X,raWWi:;X88r;SB0WB0Z2aaZZ8M ,Mi;.M8
+     BMXBBXX0aZ8WMMZSSBZMMB ;MMMMX 2MBWMMZSWM BaS822S22aaZZWMBZ0BMM@0Z88aarXaZBZ0000B822aaZ0W ZM:.Ma
+   SMMB7WZS7M;ZZ88BWWMWS0M     BMM ,WBBBM208BW@W8822SS22aaZ0M0a88B000BWBSa0Ba2B0800880Ba2aaZ0B;X X@a
+  ;MS0X;@77im7s2mxaz8wmzm       .M0@@800mr7xsaa8w@a2SSS2aaa8MM0@BBB00ZaXZ8a2BBW07BZB8800822aZ80@BZ@X
+ .BarS:7Z7riMrXXMrBW0BBMM         aMW88ZM   .... 2Z2SS222aa8MW;::aW0BBWW0W0BB0XSWW00BB080Ba22aZZ0a@;
+ BZ2W2SW,:,MrX7XWX2BZZ80M8          WMB0Mrriiiii 0Z2SS22aaZZM0BB08WWB00BWWBWWZWW080@:8B008B822aZZ8M,
+ M@XSSSS2Z8X,r;0Z7SBa8WWMM02         .SMMMM@027; WaSSS222aaZMWWWWW;X8W@B00WWB8Z880BBWW80BB000a2aZ2M
+ M2Z;: iXrSaS:287XX0Wa8M2@MM             r7WMWWBrW2SSS22aaaZ@WSWa@82;XSZW@BBW@0B22Z000008BB00082S8M
+ Ma,r8MM877S7ZB:r77@2X8WrW@MX                 X@XM2SSSZ880WMMWaBS@8W7XXSSS8WWWB00BBZ800008BBB00ZZMM
+ M0X@MMW:i2WX2XZ7;807aZM8M@MMM                   M22aa28BB88W0ZW8ZaWS7XXX7Sa2WM@B0000Z00000880MMMM;
+ iMaBMM Sr  2a722@X7;88MiW00MM                   MaaS8BZSS2aZ0@WBW0W;i7XXSXS2ZZBM@WB000000B@MMMr
+  M80WM:M     7SX2Z7X2Maa@ZMMMM                  M2S0Z2XXXX2aZ8M270@2XX77XX8r2M8aB@M@WBBW@MZ.
+   BZZMBa0BS    ZrZZ80BiW@M@W@M                  MX0Z2XX7rXS2aZB@X82WMSMM022XaM0iXZZW@M0a8M
+    MaZ@Z;  72   7r2ZaX7WM;MZMM                 M80Z2SXi  iX2aZ8MMMMM    aMW0SMM2:2SSXX28@M
+    .0ZZW@W X Bi7 202a7WMBXMMMM                ZZBZZS7:  ,72Z80BMW8BM.r     XBWWB8Z8B@MMZ
+      M;i08Z2X.MMM MSa8WB,MMWMM               MMa0aZXrir77XXXXS0M8Z8@MMM       :ZMMB2i
+       XS.ZZa0ZZ.  2MX8a,@m2mmi              XMWS0Z822Srr77X2aaZMS0aaZ8MM
+        :@ 7Z22.   MM7ZWBMX0MM               MSZrMMW2;iSZ222Z8WMMrS@,B0WMMM:
+          ra SZ0ZMM@0SZW7iWMM               iMrSiXaX7a8aaZWMMMMMX;XMaB0M2BMM
+           .MB iXaa8S7ZW2M@M                a028W@22a0@MMMM2  :irXXa87Z@.BBMW:
+              8MX    i0Ma0M                 W2Ba;XZWWB2;:.ia8;8:iX7X@;Z0aB@MMM
+                 MMMMMM@M8                  M@S8ai      .7Z77SWX;7i827aZMaBZZMr
+                    X;i;                    MBaar7WMMMMMM@8;:Zrr0a2S7XXaM B8rMM
+                                            2@8S8@MM0    ,r8@aS7SrBarXXSM:B@0MM2
+                                            ;MWX08MM S:.     ;@8x72xbxxz0i0@S@WM
+                                             ,M2a8MM @         :Ma 2S8WSZi0M MZM
+                                              M@XZBM0: 8MMB7     SM:iZa8S;8M MMM
+                                               ZWS8WMZ B    Z0  i  MS 0Z8a0M.MZM
+                                                MB;;WW8 8Mi . MiSZ XMr 8Z80X7MXM
+                                                 XW ,W8@; ZBM :MW;  MM228Z2.MZW@
+                                                  XM; S0aBX XMWX    MMB7Z0BWM:M,
+                                                   ,aW: SZX8a,    7MMZ@Sa0WZXMM
+                                                      MB.ia8aXa@MMMB7BBS20,:,X
+                                                        8W, i7B8ZZ8WMWa7Z0iX7X
+                                                         ,MMM;        XaBMa8W
+                                                             @mmm8ba00@@mmm8
+                                                                 7XiiaS.r.
+"
+
+user_motorcycle = "
+...     . . . ..................................................: ::~ .........
+....            ..............................................??+=+~:...........
+        . . . ......................................... ,.,..IIIOZ7I
+.        . . . .....................IZZZ$Z$Z$Z$ZI........I+=~$Z$I$$=~,
+.        . . . ...................$OZZZZZ$$$$$$$$$ZZ.. ..:II?$D$$$7==~,.
+.      .........................$8,OOOOOOOOOOOZZZZZZZZZ..+7II$Z$I+~+=~.
+       .........................8.Z.O8OOOOOOO88OOOOOOZO...777I77I,,~+=.
+.  .............................87.Z~ZZZZZZ8D8NDD8D=I7:...O77?=+:,,,+.. ..  ..
+..  .............................8O:ODNNMMMZZ+~:DD8=?7=?:OO877?...,:~I   ..  ..
+..  ..............................8DDNOOM$$7=~::DDD?Z?IZO888II:,.:=+?=.  .   .
+.. .............               ..DDDN8Z$7I?Z+~::7ZDZ+ZO88888DI?+?III?...........
+.. ............. . . . . . . . .8DDDNM$77=$8M+~:,,:~ZO8888DDDD7I$I..............
+.. ............. . . . . . . . .:DDDNN77II,7MNN~~~+$$O8DDDDD8...................
+.. ............. . . . . . . .Z...:DO:,,,~~:MM$==?I7$7ZO8DDD8O..................
+.. .............            .DD... .?+===?IMMM7II$O?=$ZOOOOOOOZ:................
+.. .............            . MN.. .II7$$OMMMZZ$$O87ZOZOOOOOOZ7777..............
+.. ............. . . ..... . . MN...,77ZONMMOMN7Z8O8OZOOOOOO$777777,............
+.. .............      ..,8=.  ..MN .. .OMM$$NDZOO8OZZZZOOOO$777777777...........
+.. .............        . .DD=O8DD?. ......,$Z88D8OOOZZZZZZ7777777777$..........
+.. ............. . . . . . .,D7.NNM  . . . ....8D888Z777$7777777777$$$..........
+.............................DNNNNNZ..........8DD$IIIIII777777777$$$ZZ$.........
+..........................,DDNNNND=N .......78$IIII77I7777777$$$$ZZZZZ$.........
+........................,NDDDNNNNND7N... ,~O7IIII7I7777777$$$ZZZZOOOOZ$.........
+.................. ....DDDDZO$NDDDO8NNI+I=ZII7I77777777$$$ZZZZOO888OOZZ.........
+...............Z$M:. ..D+?ZZZ8N$II???INI$7II777777777$$$ZZZOOOOO8888OOZ.........
+.................DI$MM I$$I8DOZ$7I?+??IIIII7777777$$$ZZZZZZONNNDD888O8..........
+..................,OI$ZDDO8,.8Z$7I?++ZIII77777777$$ZZZZZZNMNNNDDD88O,...........
+.........?NDDN8....$ZDDMM7,,.?8Z$7I?+ZI77777777$$ZZZZ,.MNMMMNNDD8OO.............
+......DDNDDDDDNDD7$8N.$+7Z:,,M+8Z$77I+7$77777$$ZZZZMMMD8$OOZONMMMMDZ8...........
+....?NDDDNNNND+?Z8DM..~=NMMMMMN+DOZ$7IZ$777$ZZZZZZM8O8~$O:::?ZZDD88MMMMNND?.....
+...DDDDNI7$7=+I$OMDDN8D+OMMN77NZODOZZ$7$Z7$ZZZZZM+=~O:+,IZ:ZZZ..:OOONNN888OZZ$N.
+,DDDD?ZO8=?IZOND8NDNNO+?,I.77NNM?O8OZZ$$8ZZOZZN8OO~~::,,:..$7$.ZOOZZZZZ$$$$$ZO.
+.DND8?8O~I7OZIO+D8NNNN.:7Z8,DDNN$NO7DOOOZ$$DOZDD8O~::,=$,.=NNMMMD88O888DDDDDNN7
+.ND88?I~,~$NND$:D8DDNN....DN NM,NDNM?ND88OOO8ODDDN$:ZZZNMMNNNMMNDDDDDNNNNNNNN+..
+DND8O?$IZNNNO$?ND8NMNN....$NNND8NMNMM8$NDDDNMDNNNNNMNNNNNNMMMMNNNNNNNNN? .. ....
+DND88+$$8NO$?78DDNDNNN....,NNN8NNN8NNNDDNNDDO8NMMMMMNNNMMMMNNNMMNMMM$MI.........
+.NNDDDZ=Z=77$NDDDDNNN.....NNNNNNNDNNND??8N8OZONNNDDMMNNNMNNNNNNNNNMMMND ........
+.NNNDDDDNDNNDDDDNNNNN.....NNNMMZNNNNNNNN+~:IZZNNDNNDODDDND8NDDNDNDDM. ..........
+..MDNNNDDDDDNDNNNNN........MMDNZD8D8DDZD88OZ?77I?NNDNNDDDDDDDDDDDNDNO...........
+...NNND8D8NDNNNDMN..........?MMMMMMNOOD8OOZZZZO+====?DDNNNNNNNNDDDDNDNN.........
+.....MNNNNMNNNMD............ .NDMMMMDO8ODONONNNZZOZ======77$ONNNNDDDNDNN,.......
+.... ...........................NMMMMN8N8NNDOD8NIZZZ8Z=====+Z?INNNDDDNDDND......
+....    ..........................MMMN NNN8$NMDOZZNOD$$ZZ===,ZZ=ONND8DNDDD? ....
+....    .............................O:..78MM8ONMOONZZOD$8==~$87.NND8DNDMNN.....
+....    ....................................DNDMZMNOOONOZ8==+$?.+~ND88NDDDDN....
+....     . . . .............................MND8M8D$NODNDNNNIZD7+:ND88NMNDDN....
+....     . . . .............................8NDDD8$NZO$ZDZONOMN7,NND88N8DNNNN...
+....    ....................................MNNDDDI7OZONNNNNZNZ+INDD8NNDDNDNM...
+....    ....................................DDNDDDDZID$~.NN8??77NDDD8N8DMNNNM...
+....     . . . .............................+NNNDDDDOZ?77$I?$$MNDDDDNDDDNDMNN...
+....    .....................................MDNNNDDDNN8888NNNDDDDDNDMNNNNND....
+....    ......................................MDDNNNDDDDDDDDDDDDDNNDNNNMNNDN....
+....    .......................................MNNDNNNNNNNNNNNNNNDNMNNNNNMM.....
+....     . . . .................................MMMDNDDDND77Z?+?NNNNMNNNNM......
+....     . . . ...................................MMMNMDNIID8?INMNMDNNMNM ......
+....    ...........................................,MMMMI$???D=$ODO=7N=.:,..., .
+....    ...............................................MMMMO?DMNNNMM.....,......
+....            ................................................................
+"
+
+puts mario_kart_intro
+puts "Welcome to Mario Kart!"
+puts "Who do you want to ride with?"
+puts "Options: Mario, Luigi, Toad"
+option = ""
+until ["Mario","Luigi","Toad"].include? option
+  option = gets.chomp
+  case option
+  when "Mario"
+    puts user_mario
+    puts "Itsa me Mario!  Letsa go!"
+  when "Luigi"
+    puts user_luigi
+    puts "All right! Mama Mia!"
+  when "Toad"
+    puts user_toad
+    puts "It\'s Toad, I\'m gonna win!"
+  else
+    puts "Please choose someone form the list!"
+    puts "Mario: \"Select your player! The options are Me - Mario, Luigi or Toad!\""
+  end
+end
+
+puts "Choose your vehicle!"
+puts "Options: GoKart, Motorcycle"
+option = ""
+until ["GoKart","Motorcycle"].include? option
+  option = gets.chomp
+case option
+when "GoKart"
+    puts user_kart
+    puts "Great choice! Here we go!"
+  when "Motorcycle"
+    puts user_motorcycle
+    puts "Let\'sa go!"
+  else
+    puts "Please try again: Choose a GoKart or Motorcycle"
+  end
+end
+
+puts "The race has begun and you've just got an item box!  Do you want a Star, Turtle Shell, or Bullet?"
+option = ""
+until ["Star","Turtle Shell","Bullet"].include? option
+option = gets.chomp
+  case option
+    when "Star"
+      puts "Your invincible!!!  You've knocked out everyone in your path to the finish line, You Win!"
+      puts user_win
+    when "Turtle Shell"
+      puts "You just threw your shell at Wario and knocked him out just as you reach the finish line!!! You win!"
+      puts user_win
+    when "Bullet"
+      puts "You just sped ahead to first place!  You win!"
+      puts user_win
+    else
+      puts "That isn't one of the options!! Please select a Star, Turtle Shell or Bullet"
+  end
+end
